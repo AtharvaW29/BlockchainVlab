e6ffa2ae3064e270e64adb3b9c29d3e037205af5
AtharvaW29 <atharavawgh29101@gmail.com>
2023-10-02 00:36:16 +0530

    Fourth Commit

--- contracts/Certificate.sol
+++ contracts/Certificate.sol
@@ -2,7 +2,7 @@ pragma solidity >=0.5.0;
 
 contract Certificate {
 
-    /** Struct */
+    /** Struct    **/
 
     struct Cert {
         string userName;
--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,5 +1,5 @@
 pragma solidity >=0.4.21 <0.7.0;
-
+    
 contract Migrations {
   address public owner;
   uint public last_completed_migration;
